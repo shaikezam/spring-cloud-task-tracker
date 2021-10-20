a028c093cb03c968cbf829c870a46c0941a8e45e
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2021-10-20 16:08:49 +0300

    fix serving all public files in the ui

--- task-tracker-ui/server.js
+++ task-tracker-ui/server.js
@@ -2,8 +2,18 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('public/index.html').pipe(res)
+
+  if (req.url === "/webapi/tasks" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("AAAAAAAAAAAAAAAAAAAAAA");
+  } else if (req.url === "/js/script.js" && req.method === "GET") {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('public/js/script.js').pipe(res)
+  } else if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('public/index.html').pipe(res)
+  }
+
 })
 
 server.listen(process.env.PORT || 4000)

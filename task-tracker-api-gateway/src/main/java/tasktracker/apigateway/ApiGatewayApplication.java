437fb419cfa056aad4009183d912a3cf4f340eb9
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2021-10-20 18:27:41 +0300

    added spring service discovery

---
+++ task-tracker-api-gateway/src/main/java/tasktracker/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package tasktracker.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route()
+				.build()
+	}
+
+}
